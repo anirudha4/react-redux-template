a1dd7501d4d14c54c9a8ee5fe7d4a834f9ff779e
anirudha4 <anirudhag1999@gmail.com>
2022-01-29 13:42:33 +0530

    fix: remove comments

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,6 @@ const client = axios.create({
     baseURL: API_URL
 })
 
-/** 
- * url {type: string, note: with preceeding slash /*}
- * method {type: string}
- * data {type: object}
- * headers {type: object}
- */
 
 export const api = async ({ url, method = 'get', data, headers = { 'ContentType': 'application/json' } }) => {
     return await client[method](url, data, headers)
